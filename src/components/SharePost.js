f0ffb3acf532a328949d74698497db9e13f7cb9e
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2025-04-03 16:09:08 +0200

    11

--- src/components/SharePost.js
+++ src/components/SharePost.js
@@ -13,10 +13,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserImg from "./UserImg";
 import { setPosts } from "state";
-import { useNavigate } from "react-router-dom";
 
 function SharePost({ userId }) {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [isImage, setIsImage] = useState(false);
   const [image, setImage] = useState(null);
@@ -54,112 +52,108 @@ function SharePost({ userId }) {
   return (
     <div className="dark:bg-grey-800 bg-white p-3 rounded-xl text-left">
       <div className="flex justify-between items-center">
-<<<<<<< HEAD
-        <div >
-=======
         <div>
->>>>>>> 64744f3d65d30abb608851c56634bd3d3342194e
           <UserImg image={picturePath} />
-        </div>
-        <input
-          value={post}
-          onChange={(e) => setPost(e.target.value)}
-          placeholder="what's in your mind..."
-          className="w-full ml-5 rounded-full p-2 focus:outline-none dark:bg-grey-700 bg-grey-10"
-        />
-      </div>
-      <Dropzone
-        className="mt-3"
-        acceptedFiles=".jpg,.jpeg,.png"
-        multiple={false}
-        onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <div
-            className="col-span-4 p-2 mt-3 rounded-md cursor-pointer 
-            dark:bg-grey-800 border border-grey-300"
-            {...getRootProps()}
-          >
-            <div>
-              <input {...getInputProps()} />
-              {!image ? (
-                <p className="text-grey-300">Add image Here</p>
-              ) : (
-                <div className="flex justify-between items-center">
-                  {image.name}
-                  <EditOutlinedIcon />
-                </div>
-              )}
-            </div>
-          </div>
-        )}
-      </Dropzone>
-      {image && (
-        <div className="relative mt-3">
-          <img src={URL.createObjectURL(image)} alt="" className="w-full" />
-          <DeleteOutlineOutlinedIcon
-            className="cursor-pointer text-grey-400 text-4xl absolute top-1 right-1"
-            onClick={() => setImage(null)}
+          <input
+            value={post}
+            onChange={(e) => setPost(e.target.value)}
+            placeholder="what's in your mind..."
+            className="w-full ml-5 rounded-full p-2 focus:outline-none dark:bg-grey-700 bg-grey-10"
           />
         </div>
-      )}
-      <hr className="m-3" />
-      <div className="relative flex justify-between items-center">
-        <div
-          onClick={() => setIsImage(!isImage)}
-          className="flex gap-1 items-center cursor-pointer"
+        <Dropzone
+          className="mt-3"
+          acceptedFiles=".jpg,.jpeg,.png"
+          multiple={false}
+          onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
         >
-          <AddPhotoAlternateOutlinedIcon />
-          <div>Image</div>
-        </div>
-        <div className="hidden lg:flex gap-1 items-center cursor-pointer">
-          <GifBoxOutlinedIcon />
-          <div>Clip</div>
-        </div>
-        <div className="hidden lg:flex gap-1 items-center cursor-pointer">
-          <AttachFileOutlinedIcon />
-          <div>Attachment</div>
-        </div>
-        <div className="hidden lg:flex gap-1 items-center cursor-pointer">
-          <MicNoneOutlinedIcon />
-          <div>Audio</div>
-        </div>
-        <div className="lg:hidden flex gap-1 items-center cursor-pointer">
-          <button
-            value={isOpen}
-            onClick={() => setIsOpen(!isOpen)}
-            className="cursor-pointer"
-          >
-            {isOpen ? <CloseIcon /> : <MoreHorizOutlinedIcon />}
-          </button>
-        </div>
-        {isOpen && (
-          <div
-            className="lg:hidden absolute rounded-lg top-[45px] z-10 right-[40%] 
-  w-[50%] bg-grey-100 dark:bg-grey-700 p-2 gap-3"
-          >
-            <div className="flex gap-1 items-center cursor-pointer">
-              <GifBoxOutlinedIcon />
-              <div className="">Clip</div>
+          {({ getRootProps, getInputProps }) => (
+            <div
+              className="col-span-4 p-2 mt-3 rounded-md cursor-pointer 
+            dark:bg-grey-800 border border-grey-300"
+              {...getRootProps()}
+            >
+              <div>
+                <input {...getInputProps()} />
+                {!image ? (
+                  <p className="text-grey-300">Add image Here</p>
+                ) : (
+                  <div className="flex justify-between items-center">
+                    {image.name}
+                    <EditOutlinedIcon />
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="flex gap-1 items-center cursor-pointer">
-              <AttachFileOutlinedIcon />
-              <div className="">Attachment</div>
-            </div>
-            <div className="flex gap-1 items-center cursor-pointer">
-              <MicNoneOutlinedIcon />
-              <div className="">Audio</div>
-            </div>
+          )}
+        </Dropzone>
+        {image && (
+          <div className="relative mt-3">
+            <img src={URL.createObjectURL(image)} alt="" className="w-full" />
+            <DeleteOutlineOutlinedIcon
+              className="cursor-pointer text-grey-400 text-4xl absolute top-1 right-1"
+              onClick={() => setImage(null)}
+            />
           </div>
         )}
+        <hr className="m-3" />
+        <div className="relative flex justify-between items-center">
+          <div
+            onClick={() => setIsImage(!isImage)}
+            className="flex gap-1 items-center cursor-pointer"
+          >
+            <AddPhotoAlternateOutlinedIcon />
+            <div>Image</div>
+          </div>
+          <div className="hidden lg:flex gap-1 items-center cursor-pointer">
+            <GifBoxOutlinedIcon />
+            <div>Clip</div>
+          </div>
+          <div className="hidden lg:flex gap-1 items-center cursor-pointer">
+            <AttachFileOutlinedIcon />
+            <div>Attachment</div>
+          </div>
+          <div className="hidden lg:flex gap-1 items-center cursor-pointer">
+            <MicNoneOutlinedIcon />
+            <div>Audio</div>
+          </div>
+          <div className="lg:hidden flex gap-1 items-center cursor-pointer">
+            <button
+              value={isOpen}
+              onClick={() => setIsOpen(!isOpen)}
+              className="cursor-pointer"
+            >
+              {isOpen ? <CloseIcon /> : <MoreHorizOutlinedIcon />}
+            </button>
+          </div>
+          {isOpen && (
+            <div
+              className="lg:hidden absolute rounded-lg top-[45px] z-10 right-[40%] 
+  w-[50%] bg-grey-100 dark:bg-grey-700 p-2 gap-3"
+            >
+              <div className="flex gap-1 items-center cursor-pointer">
+                <GifBoxOutlinedIcon />
+                <div className="">Clip</div>
+              </div>
+              <div className="flex gap-1 items-center cursor-pointer">
+                <AttachFileOutlinedIcon />
+                <div className="">Attachment</div>
+              </div>
+              <div className="flex gap-1 items-center cursor-pointer">
+                <MicNoneOutlinedIcon />
+                <div className="">Audio</div>
+              </div>
+            </div>
+          )}
 
-        <button
-          disabled={!post}
-          onClick={handlePost}
-          className="dark:bg-grey-600 bg-grey-50 py-1 px-4 rounded-lg dark:text-white text-grey-800 font-medium"
-        >
-          Post
-        </button>
+          <button
+            disabled={!post}
+            onClick={handlePost}
+            className="dark:bg-grey-600 bg-grey-50 py-1 px-4 rounded-lg dark:text-white text-grey-800 font-medium"
+          >
+            Post
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/UserImg.js
+++ src/components/UserImg.js
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function UserImg({image}) {
-    
+const UserImg = ({ image }) => {
   return (
     <img
-<<<<<<< HEAD
-      src={`${image}`}
-=======
       src={image}
->>>>>>> 64744f3d65d30abb608851c56634bd3d3342194e
-      alt=""
-      className="w-10 h-10 rounded-full object-cover cursor-pointer"
+      alt="User"
+      className="w-10 h-10 rounded-full object-cover"
     />
   );
-}
+};
 
-export default UserImg
+export default UserImg;
